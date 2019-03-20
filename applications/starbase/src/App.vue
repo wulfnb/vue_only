9e38eb7c5ac2f19120d0934361ec7ab47a2d99a2
NoUfAL BAbU <noufal@scientiaindia.com>
2019-03-20 13:53:40 +0530

    Completed 28

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <h3>{{title}}</h3>
+        <Character />
+    </div>
+</template>
+
+<script>
+import Character from './components/Character.vue'
+export default {
+    name: 'app',
+    data(){
+        return {
+            title : "generate  your team"
+        }
+    },
+    methods: {
+    },
+    components: {
+        Character
+    }
+}
+</script>
